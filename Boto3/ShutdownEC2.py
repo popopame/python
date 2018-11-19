1a26fa82427187bea1c19e253f06d09f2b16482e
Popopame <42341890+popopame@users.noreply.github.com>
2018-11-19 22:42:43 +0100

    Delete ShutdownEC2.py

--- Boto3/ShutdownEC2.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-ec2 = boto3.resource('ec2')
-instances = ec2.instances.filter(Filters=[{'Name': 'tag:group', 'Values': ['docker_host']}])
-
-for instance in instances:
-    print(instance.id)
-
